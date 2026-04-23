c319028dc407343a08063da863f25ae4a99f79f9
Programuddin <hakmulok@gmail.com>
2026-04-22 20:36:47 -0500

    Gun is now fully implemented

--- Assets/Scripts/PickupScripts/Gun.cs
+++ Assets/Scripts/PickupScripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 public class Gun : MonoBehaviour, IUsableItem
 {
     public GameObject bulletPrefab;
@@ -11,10 +12,18 @@ public class Gun : MonoBehaviour, IUsableItem
 
     public void UseItem()
     {
-        if(bulletPrefab != null)
-            Instantiate(bulletPrefab, firedFrom.position, firedFrom.rotation);
-        else
-            Debug.Log("No prefab assigned to bulletPrefab");
-        Debug.Log("Gun was used");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        Vector2 shootDirection = (mousePos - firedFrom.position).normalized;
+        float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg + 90;
+        Quaternion bulletRotation = Quaternion.Euler(0,0, angle);
+        
+        GameObject bullet = Instantiate(bulletPrefab, firedFrom.position, bulletRotation);
+
+        BulletLogic bulletScript = bullet.GetComponent<BulletLogic>();
+        if(bulletScript != null)
+        {
+            bulletScript.Shoot(shootDirection);
+        }
     }   
 }
